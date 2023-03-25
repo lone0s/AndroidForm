102a78f94c075bdb5c55271a03b38db05a57239d
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2023-03-25 13:01:53 +0100

    RadioButtonQuestion OK

--- app/src/main/java/com/example/androidform/questions/lib/RadioButtonQuestion.java
+++ app/src/main/java/com/example/androidform/questions/lib/RadioButtonQuestion.java
@@ -11,22 +11,26 @@ import androidx.annotation.NonNull;
 
 import com.example.androidform.core.Language;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 public class RadioButtonQuestion extends Question<Short> {
 
-    private RadioButtonQuestion(RadioGroup radioGroup, Collection<Pair<Language, Short>> values) {
-        super((View) radioGroup, values);
+    private final List<RadioButton> answers;
+
+    private RadioButtonQuestion(@NonNull RadioGroup radioGroup, @NonNull final Collection<RadioButton> radiobuttons) {
+        super((View) radioGroup);
+        this.answers = (List<RadioButton>) radiobuttons;
     }
 
+    @NonNull
     public static RadioButtonQuestion build(
             Context context,
-            @NonNull
-                    String question,
-            @NonNull
-                    HashMap<String, Pair<Language, Short>> answers
+            @NonNull String question,
+            @NonNull HashMap<String, Pair<Language, Short>> answers
     )
     {
         RadioGroup radioGroup = new RadioGroup(context);
@@ -36,23 +40,35 @@ public class RadioButtonQuestion extends Question<Short> {
 
         radioGroup.addView(label);
 
+        List<RadioButton> buttons = new ArrayList<>();
+
         answers.forEach(
                 (lbl, couple) ->
                 {
                     RadioButton button = new RadioButton(context);
                     button.setText(lbl);
                     radioGroup.addView(button);
+
+                    buttons.add(button);
                 }
         );
 
 
-        return new RadioButtonQuestion(radioGroup, answers.values());
+        return new RadioButtonQuestion(radioGroup, buttons);
     }
 
     @NonNull
     @Override
-    public Short getAnsweredText() {
-        // TODO
-        return null;
+    public Short getAnswer() {
+        if (!((RadioButton) this.target).isChecked()) return -1;
+
+        Short i = 0;
+        for (RadioButton answer : this.answers) {
+            if (answer.isChecked()) return i;
+
+            i++;
+        }
+
+        throw new Error("Dead code reached");
     }
 }
