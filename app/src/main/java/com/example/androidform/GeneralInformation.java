7e7bce2c1c0a2cd18fa0c189d352cc984ec3bae3
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2023-03-25 16:25:40 +0100

    Ajout loggers sur les eventlisteners

--- app/src/main/java/com/example/androidform/GeneralInformation.java
+++ app/src/main/java/com/example/androidform/GeneralInformation.java
@@ -1,11 +1,12 @@
 package com.example.androidform;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 
 public class GeneralInformation extends Template {
+
+    private static final String LOGGER_TAG = "General_Information";
 
     public GeneralInformation() {
         super(R.layout.activity_general_information);
@@ -13,11 +14,18 @@ public class GeneralInformation extends Template {
 
     @Override
     protected void backButtonEventListener(View evt) {
-        
+        Log.d(GeneralInformation.LOGGER_TAG, "Previous button clicked");
+        Log.e(GeneralInformation.LOGGER_TAG, "NEW VIEW CREATED, NOT OPTIMAL");
+        Intent intent = new Intent(this, MainActivity.class);
+
+        this.startActivity(intent);
     }
 
     @Override
     protected void nextButtonEventListener(View evt) {
-
+        Log.d(GeneralInformation.LOGGER_TAG, "Next button clicked");
+//        Intent intent = new Intent(this, GeneralInformation.class);
+//
+//        this.startActivity(intent);
     }
 }
