c618412be5c8d372f2e05b5556be525b55b7c3a6
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2023-03-26 14:37:53 +0200

    Fill DataContainer constructor (Parcel)

--- app/src/main/java/com/example/androidform/DataContainer.java
+++ app/src/main/java/com/example/androidform/DataContainer.java
@@ -3,38 +3,56 @@ package com.example.androidform;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import androidx.annotation.NonNull;
+public final class DataContainer implements Parcelable {
+//    private @interface ParcelContent{}
 
-import java.util.Collection;
+    /**
+     * length = 11;
+     * index 0 = rust
+     * index 1 = haskell
+     * index 2 = caml
+     * index 3 = malboge
+     * index 4 = cpp
+     * index 5 = python
+     * index 6 = r
+     * index 7 = js
+     * index 8 = php
+     * index 9 = java
+     * index 10 = cs
+     */
+    private final int[][] scores; // length = 11
+    private final int questionIndex;
+//    @ParcelContent private int[] rustScores;
+//    @ParcelContent private int[] haskellScores;
+//    @ParcelContent private int[] camlScores;
+//    @ParcelContent private int[] malbogeScores;
+//    @ParcelContent private int[] cppScores;
+//    @ParcelContent private int[] pythonScores;
+//    @ParcelContent private int[] rScores;
+//    @ParcelContent private int[] jsScores;
+//    @ParcelContent private int[] phpScores;
+//    @ParcelContent private int[] javaScores;
+//    @ParcelContent private int[] csScores;
 
-public class DataContainer implements Parcelable {
-
-    // Des collections pour manage les retours en arriere
-    private Collection<Short> rustScores;
-    private Collection<Short> haskellScores;
-    private Collection<Short> camlScores;
-    private Collection<Short> malbogeScores;
-    private Collection<Short> cppScores;
-    private Collection<Short> pythonScores;
-    private Collection<Short> rScores;
-    private Collection<Short> jsScores;
-    private Collection<Short> phpScores;
-    private Collection<Short> javaScores;
-    private Collection<Short> csScores;
-
-    protected DataContainer(Parcel in) {
+    public DataContainer(Parcel in)
+    {
+        this.questionIndex = in.readInt() +1;
+        this.scores = new int[11][this.questionIndex];
+        for (int[] score : this.scores) {
+            in.readIntArray(score);
+        }
     }
 
-    public void addRustScore(final short score) // Simplifier en addScore(final Langage lang, final short score) ?
+    public void addScore(final int score) // Simplifier en addScore(final Langage lang, final short score) ?
     {
-        this.rustScores.add(score);
+//        this.rustScores[this.rustScores.length -1] = score;
     }
 
     public short computeResults()
     {
         // Stream::reduce marche po
         int reduce = 0;
-        for (Short rustScore : this.rustScores) {
+        for (Integer rustScore : this.scores[0]) {
             reduce = reduce + rustScore;
         }
 
@@ -57,11 +75,13 @@ public class DataContainer implements Parcelable {
     };
 
     @Override
-    public int describeContents() {
+    public int describeContents()
+    {
         return 0;
     }
 
     @Override
-    public void writeToParcel(Parcel parcel, int i) {
+    public void writeToParcel(Parcel parcel, int i)
+    {
     }
 }
